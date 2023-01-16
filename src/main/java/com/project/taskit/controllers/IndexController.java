3de605519b0e0e3eb3e9d4ee874c5096b4e2dfc0
Anthony Stevens <aastevens1126@gmail.com>
2023-01-16 12:59:13 -0600

    partial updated with CSS file included

--- src/main/java/com/project/taskit/controllers/IndexController.java
+++ src/main/java/com/project/taskit/controllers/IndexController.java
@@ -1,4 +1,16 @@
 package com.project.taskit.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+
+    @GetMapping("/")
+    public String getLandingPage(){
+        return "landingPage";
+    }
+
+
 }
